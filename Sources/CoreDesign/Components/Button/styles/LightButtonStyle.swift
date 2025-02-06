429ca60038329213b5c111d16028cb5707a64d2a
Evan <coder.wxlpp@gmail.com>
2025-02-06 23:50:30 +0800

    feat: 更新按钮样式以支持不同控制大小的边距和字体

--- Sources/CoreDesign/Components/Button/styles/LightButtonStyle.swift
+++ Sources/CoreDesign/Components/Button/styles/LightButtonStyle.swift
@@ -11,6 +11,7 @@ import SwiftUI
 public struct LightButtonStyle: PrimitiveButtonStyle {
     @GestureState private var isPressed = false
     @Environment(\.isEnabled) private var isEnabled
+    @Environment(\.controlSize) private var controlSize
 
     let role: ButtonRoleStyleRole
 
@@ -23,13 +24,38 @@ public struct LightButtonStyle: PrimitiveButtonStyle {
 
     public func makeBody(configuration: Configuration) -> some View {
         configuration.label
-            .padding(EdgeInsets(top: 6, leading: 12, bottom: 6, trailing: 12))
+            .font(self.textFont)
+            .padding(self.edgeInsets)
             .foregroundStyle(self.textColor)
             .background(Color.tertiaryFill)
             .clipShape(Capsule(style: .continuous))
             .animation(.easeInOut, value: self.isPressed)
             .simultaneousGesture(self.pressedStateGesture)
             .onTapGesture(count: 1, perform: configuration.trigger)
+    }
+
+    private var edgeInsets: EdgeInsets {
+        switch self.controlSize {
+        case .mini: EdgeInsets(top: 2, leading: 12, bottom: 2, trailing: 12)
+        case .small: EdgeInsets(top: 2, leading: 12, bottom: 2, trailing: 12)
+        case .regular: EdgeInsets(top: 6, leading: 12, bottom: 6, trailing: 12)
+        case .large: EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16)
+        case .extraLarge: EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
+        @unknown default:
+            EdgeInsets(top: 6, leading: 12, bottom: 6, trailing: 12)
+        }
+    }
+
+    private var textFont: Font {
+        switch self.controlSize {
+        case .mini: .systemCaption
+        case .small: .systemCallout
+        case .regular: .systemBody
+        case .large: .systemTitle3
+        case .extraLarge: .systemTitle
+        @unknown default:
+            .systemBody
+        }
     }
 
     private var textColor: Color {
@@ -54,6 +80,7 @@ extension PrimitiveButtonStyle where Self == SolidButtonStyle {
             Text("Login")
         }
         .buttonStyle(.lightButton(role: .primary))
+        .controlSize(.mini)
 
         Button(action: {
             // 注册按钮的操作
@@ -61,6 +88,7 @@ extension PrimitiveButtonStyle where Self == SolidButtonStyle {
             Text("Register")
         }
         .buttonStyle(.lightButton(role: .secondary))
+        .controlSize(.small)
 
         Button(action: {
             // 忘记密码按钮的操作
@@ -68,6 +96,7 @@ extension PrimitiveButtonStyle where Self == SolidButtonStyle {
             Text("Forgot Password")
         }
         .buttonStyle(.lightButton(role: .warning))
+        .controlSize(.regular)
 
         Button(action: {
             // 提交按钮的操作
@@ -75,6 +104,7 @@ extension PrimitiveButtonStyle where Self == SolidButtonStyle {
             Text("Submit")
         }
         .buttonStyle(.lightButton(role: .danger))
+        .controlSize(.large)
 
         Button(action: {
             // 取消按钮的操作
@@ -82,5 +112,6 @@ extension PrimitiveButtonStyle where Self == SolidButtonStyle {
             Text("Cancel")
         }
         .buttonStyle(.lightButton(role: .tertiary))
+        .controlSize(.extraLarge)
     }
 }
--- Sources/CoreDesign/Components/Button/styles/SolidButtonStyle.swift
+++ Sources/CoreDesign/Components/Button/styles/SolidButtonStyle.swift
@@ -11,6 +11,7 @@ import SwiftUI
 public struct SolidButtonStyle: PrimitiveButtonStyle {
     @GestureState private var isPressed = false
     @Environment(\.isEnabled) private var isEnabled
+    @Environment(\.controlSize) private var controlSize
 
     let role: ButtonRoleStyleRole
 
@@ -23,7 +24,8 @@ public struct SolidButtonStyle: PrimitiveButtonStyle {
 
     public func makeBody(configuration: Configuration) -> some View {
         configuration.label
-            .padding(EdgeInsets(top: 6, leading: 12, bottom: 6, trailing: 12))
+            .font(self.textFont)
+            .padding(self.edgeInsets)
             .foregroundStyle(Color.white)
             .background(self.backgroundColor)
             .clipShape(Capsule(style: .continuous))
@@ -37,6 +39,30 @@ public struct SolidButtonStyle: PrimitiveButtonStyle {
             return self.role.disabledColor
         }
         return self.isPressed ? self.role.activeColor : self.role.color
+    }
+
+    private var edgeInsets: EdgeInsets {
+        switch self.controlSize {
+        case .mini: EdgeInsets(top: 2, leading: 12, bottom: 2, trailing: 12)
+        case .small: EdgeInsets(top: 2, leading: 12, bottom: 2, trailing: 12)
+        case .regular: EdgeInsets(top: 6, leading: 12, bottom: 6, trailing: 12)
+        case .large: EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16)
+        case .extraLarge: EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
+        @unknown default:
+            EdgeInsets(top: 6, leading: 12, bottom: 6, trailing: 12)
+        }
+    }
+
+    private var textFont: Font {
+        switch self.controlSize {
+        case .mini: .systemCaption
+        case .small: .systemCallout
+        case .regular: .systemBody
+        case .large: .systemTitle3
+        case .extraLarge: .systemTitle
+        @unknown default:
+            .systemBody
+        }
     }
 }
 
@@ -54,6 +80,7 @@ extension PrimitiveButtonStyle where Self == SolidButtonStyle {
             Text("Login")
         }
         .buttonStyle(.solidButton(role: .primary))
+        .controlSize(.mini)
 
         Button(action: {
             // 注册按钮的操作
@@ -61,6 +88,7 @@ extension PrimitiveButtonStyle where Self == SolidButtonStyle {
             Text("Register")
         }
         .buttonStyle(.solidButton(role: .secondary))
+        .controlSize(.small)
 
         Button(action: {
             // 忘记密码按钮的操作
@@ -68,6 +96,7 @@ extension PrimitiveButtonStyle where Self == SolidButtonStyle {
             Text("Forgot Password")
         }
         .buttonStyle(.solidButton(role: .warning))
+        .controlSize(.regular)
 
         Button(action: {
             // 提交按钮的操作
@@ -75,6 +104,7 @@ extension PrimitiveButtonStyle where Self == SolidButtonStyle {
             Text("Submit")
         }
         .buttonStyle(.solidButton(role: .danger))
+        .controlSize(.large)
 
         Button(action: {
             // 取消按钮的操作
@@ -82,5 +112,6 @@ extension PrimitiveButtonStyle where Self == SolidButtonStyle {
             Text("Cancel")
         }
         .buttonStyle(.solidButton(role: .tertiary))
+        .controlSize(.extraLarge)
     }
 }
