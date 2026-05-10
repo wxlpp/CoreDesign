a97d2f1b015151d472f30b3c080df3f60dc3141e
Evan <coder.wxlpp@gmail.com>
2026-05-10 21:42:53 +0800

    Revert "Merge epic: storybook-app (#52)"
    This reverts commit 0f9042ea226805478288382881be37005ba61173, reversing
    changes made to 7adfdf97a51d17a4f299d1d6e48785a2ea9c6fd4.

--- App/Tests/SnapshotTests.swift
+++
@@ -1,9 +0,0 @@
-import SnapshottingTests
-import XCTest
-
-final class SnapshotTests: SnapshotTest {
-    // 默认收编 App target 中所有 #Preview 宏生成 PNG。
-    override class func excludedSnapshotPreviews() -> [String]? {
-        ["ContentView_Preview"]
-    }
-}
